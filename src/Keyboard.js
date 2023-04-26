21db756349cce286afca320ea9717abdefbcb140
Maria Selivanova <maria.m.selivanova@yandex.ru>
2023-04-26 01:38:04 -0500

    feat: add basic functionality to Keyboard class

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import './styles/index.css';
 import { keyLayoutEn, keyLayoutRu } from './utils/keyLayout';
 
@@ -8,5 +9,95 @@ export default class Keyboard {
     this.keys = [];
     this.value = '';
     this.capsLock = false;
+    this.keyboardInput = document.querySelector('.keyboard-input');
+  }
+
+  init() {
+    this.container = document.createElement('div');
+    this.container.classList.add('keyboard');
+    this.container.append(this._makeKey());
+    this.keys = this.container.querySelectorAll('.keyboard__key');
+    document.body.append(this.container);
+  }
+
+  updateInput() {
+    this.keyboardInput.value = this.value;
+  }
+
+  _makeKey() {
+    const fragment = document.createDocumentFragment();
+    this.alphabet.forEach((key) => {
+      const keyElement = document.createElement('button');
+      const insertLineBreak = ['backspace', 'del', 'enter', 'shiftRight', 'ctrlRight'].indexOf(key) !== -1;
+
+      keyElement.setAttribute('type', 'button');
+      keyElement.classList.add('keyboard__key');
+
+      switch (key) {
+        case 'enter':
+          keyElement.classList.add('keyboard__key_wide');
+          keyElement.innerHTML = 'Enter';
+          keyElement.addEventListener('click', () => {
+            this.value += '\n';
+            this.updateInput();
+          });
+          break;
+
+        case 'space':
+          keyElement.classList.add('keyboard__key_super-wide');
+          keyElement.addEventListener('click', () => {
+            this.value += ' ';
+            this.updateInput();
+          });
+          break;
+
+        case 'backspace':
+          keyElement.classList.add('keyboard__key_wide');
+          keyElement.textContent = 'Backspace';
+          keyElement.addEventListener('click', () => {
+            this.value = this.value.slice(0, this.value.length - 1);
+            this.updateInput();
+          });
+          break;
+
+        case 'caps':
+          keyElement.classList.add('keyboard__key_wide');
+          keyElement.textContent = 'CapsLock';
+
+          keyElement.addEventListener('click', () => {
+            this._toggleCapsLock();
+          });
+          break;
+
+        default:
+          keyElement.textContent = key;
+          keyElement.addEventListener('click', () => {
+            this.value += this.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this.updateInput();
+          });
+          break;
+      }
+
+      fragment.appendChild(keyElement);
+
+      if (insertLineBreak) {
+        fragment.appendChild(document.createElement('br'));
+      }
+    });
+
+    return fragment;
+  }
+
+  _toggleCapsLock() {
+    this.capsLock = !this.capsLock;
+    this.keys.forEach((item) => {
+      if (item.textContent.length === 1) {
+        if (this.capsLock) {
+          item.textContent = item.textContent.toUpperCase();
+        } else {
+          item.textContent = item.textContent.toLowerCase();
+        }
+      }
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 /* eslint-disable no-undef */
 import Keyboard from './Keyboard';
 
+// заголовок
+const header = document.createElement('h1');
+header.textContent = 'RSS Виртуальная клавиатура';
+document.body.append(header);
+
+// инпут
+const keyboardInput = document.createElement('textarea');
+keyboardInput.classList.add('keyboard-input');
+document.body.append(keyboardInput);
+
+// подпись
+const text = document.createElement('p');
+text.classList.add('text');
+text.textContent = 'Клавиатура создана в операционной системе Windows. Для переключения языка комбинация: левыe ctrl + alt';
+document.body.append(text);
+
 const keyboard = new Keyboard();
 window.addEventListener('DOMContentLoaded', () => {
   keyboard.init();
